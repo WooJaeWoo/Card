d31b234908f650990f1159b8953e2a4d34698077
Jaewoo Woo <gewoose@gmail.com>
2015-09-04 15:11:36 +0900

    0904_2

--- card.js
+++ card.js
@@ -1,95 +1,128 @@
+//service
 $(document).ready(function() {
 	CARD.init();
 });
 
 var UTIL = {
-	"PAGE_COUNT" : 1,
-    boxTemplate : function(boxData) {
-        return {
-            "id" : boxData[0],
-            "grid" : {
-                "col" : boxData[1],
-                "row" : boxData[2],
-                "margin" : boxData[3]
-            },
-            "text" : {
-                "contents" : boxData[4],
-                "length" : boxData[5],
-                "tag" : boxData[6]
-            },
-            "from" : boxData[7]
+    rand : function(limit) {
+        return Math.floor(Math.random() * limit);
+    },
+    isMobile : function() {
+        var isMobile = false;
+        // device detection
+ if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
+    || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) {
+            isMobile = true;
         }
+        return isMobile;
     }
 };
-
-//TODO: box 개수가 바뀔 수도 있음...
-
-//boxTemplate([id, grid.col, grid.row, grid.margin, text.content, text.length, text.tag, from]);
-var INFO = {
-	"page1" : {
-		"bgColor" : "#FF9F93",
-        "angle" : "18deg",
-		"box1" : UTIL.boxTemplate(["box1", "c3", "r3", "", "안녕", 0, "nobr", "left"]),
-		"box2" : UTIL.boxTemplate(["box2", "c1", "r3", "", "반가워", 0, "span", "top"]),
-		"box3" : UTIL.boxTemplate(["box3", "c4", "r1", "", "헬로월드", 0, "nobr", "bottom"])
-	},
-    "page2" : {
-		"bgColor" : "#CDAFA3",
-        "angle" : "-12deg",
-        "box1" : UTIL.boxTemplate(["box1", "c1", "r4", "", "시간차를", 0, "span", "left"]),
-		"box2" : UTIL.boxTemplate(["box2", "c3", "r1", "", "어떻게", 0, "nobr", "top"]),
-		"box3" : UTIL.boxTemplate(["box3", "c3", "r3", "", "둘것인가", 0, "nobr", "right"])
-	},
-    "page3" : {
-		"bgColor" : "#D2D111",
-        "angle" : "-12deg",
-        "box1" : UTIL.boxTemplate(["box1", "c4", "r1", "", "자바스크립트", 0, "nobr", "right"]),
-		"box2" : UTIL.boxTemplate(["box2", "c2", "r3", "", "엄청", 0, "nobr", "left"]),
-		"box3" : UTIL.boxTemplate(["box3", "c2", "r3", "", "재밌다", 0, "nobr", "bottom"])
-	}
-};
-
+//BOX.boxTemplate([id, grid.col, grid.row, grid.margin, text.content, text.length, text.tag, from]);
 
 var CARD = {
 	init : function() {
 		console.log("START");
-		var pages = Object.keys(INFO).length;
-		$("main").on("click", function() {
-			console.log("PAGE: " + UTIL.PAGE_COUNT);
-			this.clearPage();
-			this.setPage(INFO["page" + UTIL.PAGE_COUNT]);
-			UTIL.PAGE_COUNT++;
-			
-			//END
-			if (UTIL.PAGE_COUNT > pages) {
-				$("main").off()
-				console.log("END");
+        PAGE.init();
+
+		$("body").on("click", function() {
+			if (PAGE.PAGE_COUNT === 0) {
+                $("nav").addClass("blur");
+                $("nav").one("trasitionend", function() {
+                    $("nav").css("display", "none");
+                });
+            } else {
+                PAGE.clearPage();
+            }
+            
+            //END condition
+            if (PAGE.PAGE_COUNT >= PAGE.getTotalPage()) {
+                this.endEvent();
+                return;
 			}
+            
+            PAGE.PAGE_COUNT++;
+            console.log("PAGE: " + PAGE.PAGE_COUNT);
+            
+            PAGE.setPage(CONTENTS["page" + PAGE.PAGE_COUNT]);
+            
 		}.bind(this));
 	},
-	setPage : function(page) {
+    endEvent : function() {
+        $("body").off();
+        $("nav").css("display", "block");
+        $("nav").removeClass("blur");
+        $("h3").text("END");
+		$("#retryButton").css("display", "inline").one("click", function() {
+            this.init();
+        }.bind(this));
+        console.log("END");
+    }
+};
+
+//TODO: vertical = "span", horisontal = "nobr"
+
+var PAGE = {
+    PAGE_COUNT : 0,
+    PAGE_COLORS : ["#FF9F93", "#EF9A9A", "#CDAFA3", "#E1BEE7", "#9FA8DA", "#90CAF9", "#DCE775", "#FFCC80", "#FFCA28", "#FFAB91"],
+    init : function() {
+        var startText = "";
+        if (UTIL.isMobile()) {
+            startText = "TOUCH TO START";
+        } else {
+            startText = "CLICK TO START";
+        }
+        this.PAGE_COUNT = 0;
+        $("h3").text(startText);
+        
+        var nav = $("nav");
+        nav.removeClass("blur");
+        
+        $("#retryButton").css("display", "none");
+    },
+    getTotalPage : function() {
+        return Object.keys(CONTENTS).length;
+    },
+    clearPage : function() {
+		for (var i = 1; i <= BOX.getBoxCount(this.PAGE_COUNT); i++) {
+            $("#box" + i).removeClass();
+            $("#box" + i + " p").empty();
+            $("#box" + i).css("animation-name");
+		}
+        //TODO: out animation
+	},
+    setPage : function(page) {
 		//Page Background
 		this.setBgColor(page.bgColor);
         this.setAngle(page.angle);
         
 		//each Box setting
-		for (var i = 1; i <= 3; i++) {
-			this.setBox(page["box" + i]);
+		for (var i = 1; i <= BOX.getBoxCount(this.PAGE_COUNT); i++) {
+			BOX.setBox(page["box" + i]);
 		}
 	},
-	setBgColor : function(bgColor) {
-		$("main").css("background-color", bgColor);
+    setBgColor : function() {
+        var colorIndex = UTIL.rand(this.PAGE_COLORS.length);
+		$("main").css("background-color", this.PAGE_COLORS[colorIndex]);
 	},
     setAngle : function(angle) {
         $(".cardGrid").css("transform", "rotate(" + angle + ")");
+    }
+}
+
+var BOX = {
+    getBoxCount : function(pageNum) {
+        return Object.keys(CONTENTS["page" + pageNum]).length;
     },
-	setBox : function(boxInfo) {
+    setBox : function(boxInfo) {
 		var targetBox = $("#" + boxInfo.id);
         this.setDirectionFrom(targetBox, boxInfo.from);
 		this.setText(targetBox.children(), boxInfo.text);
 		this.setGrid(targetBox, boxInfo.grid);
 		this.scaleText(targetBox.children(), boxInfo);
 	},
+    setTextDirection : function() {
+        
+    },
     setDirectionFrom : function(box, direction) {
         box.addClass("from" + direction);
     },
@@ -110,15 +143,41 @@ var CARD = {
         if (boxInfo.text.tag === "span") {
             text.css("transform", "scaleY(" + row/textLenghth + ")");
         } else {
-            text.css("transform", "scale(" + col/textLenghth + "," + row + ")").css("padding", "2px 4px");
+            text.css("transform", "scale(" + col/textLenghth + "," + row + ")").css("padding", "2px 3px");
         }
 	},
-	clearPage : function() {
-		for (var i = 1; i <= 3; i++) {
-            $("#box" + i).removeClass();
-            $("#box" + i + " p").empty();
-            $("#box" + i).css("animation-name");
-		}
-        //TODO: easeout animation
+    boxTemplate : function(boxData) {
+        return {
+            "id" : boxData[0],
+            "grid" : {
+                "col" : boxData[1],
+                "row" : boxData[2],
+                "margin" : boxData[3]
+            },
+            "text" : {
+                "contents" : boxData[4],
+                "length" : boxData[5],
+                "tag" : boxData[6]
+            },
+            "from" : boxData[7]
+        }
+    }
+}
+
+var CONTENTS = {
+    "page1" : {
+		"box1" : BOX.boxTemplate(["box1", "c3", "r3", "", "안녕", 0, "nobr", "left"]),
+		"box2" : BOX.boxTemplate(["box2", "c1", "r3", "", "반가워", 0, "span", "top"]),
+		"box3" : BOX.boxTemplate(["box3", "c4", "r1", "", "헬로월드", 0, "nobr", "bottom"])
+	},
+    "page2" : {
+        "box1" : BOX.boxTemplate(["box1", "c1", "r4", "", "시간차를", 0, "span", "left"]),
+		"box2" : BOX.boxTemplate(["box2", "c3", "r1", "", "어떻게", 0, "nobr", "top"]),
+		"box3" : BOX.boxTemplate(["box3", "c3", "r3", "", "둘것인가", 0, "nobr", "right"])
+	},
+    "page3" : {
+        "box1" : BOX.boxTemplate(["box1", "c4", "r1", "", "자바스크립트", 0, "nobr", "right"]),
+		"box2" : BOX.boxTemplate(["box2", "c2", "r3", "", "엄청", 0, "nobr", "left"]),
+		"box3" : BOX.boxTemplate(["box3", "c2", "r3", "", "재밌다", 0, "nobr", "bottom"])
 	}
 };
